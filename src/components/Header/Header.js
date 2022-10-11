befd6a7b98fb5b5b05d6b1011b2e08beef353288
Mohammad Ayub <5043ayub@gmail.com>
2022-10-11 08:47:48 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="logo">
+                <Link to='/'><h3>Quiz Maker</h3></Link>
+            </div>
+            <nav>
+                <ul>
+                    <Link to='/'>Home</Link>
+                    <Link to='/topics'>Topics</Link>
+                    <Link to='/statistics'>Statistics</Link>
+                    <Link to='/blog'>Blog</Link>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+	return (
+		<div className='home'>
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-12">
+						<div className="home-cnt">
+							<h2> Welcome to the quiz page.</h2>
+							<p>It helps in identifying the students with what they know and what they don't. It increases the level of practice, and concentration and increases the uplift of the memorizing level at its best.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;

cda36fcf7efd2219696b611b17d880727b7da586
Mohammad Ayub <5043ayub@gmail.com>
2022-10-12 23:53:52 +0600

    Responsive style

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
 import './Statistics.css';
-import { LineChart, CartesianGrid, Tooltip, Legend, Line, Bar, XAxis, YAxis, ResponsiveContainer } from 'recharts';
+import { LineChart, CartesianGrid, Tooltip, Legend, Line, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 import { useLoaderData } from 'react-router-dom';
 
 const Statistics = () => {
 	const quizData = useLoaderData();
 	const quiz = quizData.data;
-		
+		console.log(quiz)
 	return (
 		<div>
 			<div className="statistics">
@@ -20,6 +20,7 @@ const Statistics = () => {
 						</div>
 						<div className="chart-container">
 							<div className="row">
+								
 								<LineChart width={800} height={350} data={quiz}
 									margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
 									<CartesianGrid strokeDasharray="3 3" />
@@ -29,7 +30,8 @@ const Statistics = () => {
 									<Legend />
 									<Line type="monotone" dataKey='name' stroke="#8884d8" />
 									<Line type="monotone" dataKey="total" stroke="#82ca9d" />
-								</LineChart>			
+								</LineChart>
+											
 							</div>
 						</div>
 					</div>

4136ae5e8ccfb5b30e23e97b9a942da565b4bc00
Mohammad Ayub <5043ayub@gmail.com>
2022-10-12 22:38:06 +0600

    Rechart added

--- src/App.js
+++ src/App.js
@@ -32,6 +32,9 @@ function App() {
           element:<Blog></Blog>
         },{
           path:'/statistics',
+          loader: async () => {
+            return fetch('https://openapi.programming-hero.com/api/quiz')
+          },
           element:<Statistics></Statistics>
         },
         {
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
 import './Statistics.css';
+import { LineChart, CartesianGrid, Tooltip, Legend, Line, Bar, XAxis, YAxis, ResponsiveContainer } from 'recharts';
+import { useLoaderData } from 'react-router-dom';
 
 const Statistics = () => {
+	const quizData = useLoaderData();
+	const quiz = quizData.data;
+		
 	return (
 		<div>
 			<div className="statistics">
@@ -10,7 +15,21 @@ const Statistics = () => {
 					<div className="row">
 						<div className="col-lg-12">
 							<div className="statistics-header">
-								<h3>Quiz Statistics.</h3>
+								<h3>Quiz topic Statistics.</h3>
+							</div>
+						</div>
+						<div className="chart-container">
+							<div className="row">
+								<LineChart width={800} height={350} data={quiz}
+									margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+									<CartesianGrid strokeDasharray="3 3" />
+									<XAxis dataKey="name" />
+									<YAxis />
+									<Tooltip />
+									<Legend />
+									<Line type="monotone" dataKey='name' stroke="#8884d8" />
+									<Line type="monotone" dataKey="total" stroke="#82ca9d" />
+								</LineChart>			
 							</div>
 						</div>
 					</div>
